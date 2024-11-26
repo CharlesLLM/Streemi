262814b9c2fdb3d5b05d885aa8d161a2924f79d2
CharlesLLM <charlesllm@orange.fr>
2024-11-26 16:17:13 +0100

    lint
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -6,9 +6,6 @@ use App\Repository\SerieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
-use Symfony\Bridge\Doctrine\Types\UuidType;
-use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: SerieRepository::class)]
 class Serie extends Media
